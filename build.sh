4f3a5537057073c0f8a21f10b137c012cd3f2041
AlvinoRD <111202214600@mhs.dinus.ac.id>
2026-01-04 22:09:12 +0700

    Fix: Remove cd command from build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,7 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Navigate to code directory
-cd code
-
-# Collect static files
+# Collect static files (already in /code from WORKDIR)
 python manage.py collectstatic --no-input
 
 # Run migrations
